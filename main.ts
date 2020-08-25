ef4fd4f76a0e0e551febc88b78bb4915da042777
Sandor-Tamas <70057013+Sandor-Tamas@users.noreply.github.com>
2020-08-25 13:15:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+serial.onDataReceived(serial.delimiters(Delimiters.Comma), function () {
+    vett = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    basic.showNumber(vett)
+})
+let vett = ""
 serial.redirect(
 SerialPin.P0,
 SerialPin.P1,
@@ -6,6 +11,7 @@ BaudRate.BaudRate115200
 let i = 0
 basic.forever(function () {
     serial.writeNumber(i)
+    serial.writeString(",")
     basic.pause(1000)
     i += 1
 })
